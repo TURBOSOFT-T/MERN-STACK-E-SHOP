1930268ccc0ddf65a32c2d48173e0b3051354988
tuemo thomas <tuemothomas@gmail.com>
2023-04-16 13:02:45 +0200

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <p className="text-[#000]" >
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
