8cd694c69487fb3a0378867fbea2499b639587db
tuemo thomas <tuemothomas@gmail.com>
2023-04-22 22:30:39 +0200

    update env

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,4 +1,4 @@
-export const server = "https://admin-backend-wtnw.onrender.com/";
+export const server = "http://admin-backend-wtnw.onrender.com/";
 
 
 export const backend_url = "http://localhost:8000/";
